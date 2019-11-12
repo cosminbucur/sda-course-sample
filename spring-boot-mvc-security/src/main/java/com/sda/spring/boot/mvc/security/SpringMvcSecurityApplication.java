76377766fb963af667ba299229e7faec5c53f191
cosmin_bucur <cosmindanbucur@gmail.com>
2019-11-13 00:04:52 +0200

    add spring security WIP

---
+++ spring-boot-mvc-security/src/main/java/com/sda/spring/boot/mvc/security/SpringMvcSecurityApplication.java
@@ -0,0 +1,12 @@
+package com.sda.spring.boot.mvc.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvcSecurityApplication.class, args);
+    }
+}
