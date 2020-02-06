37495558be63c0822b60bc454c24ea2a7f0c2156
cosmin bucur <cosmin.bucur@kambi.com>
2020-02-06 12:05:00 +0200

    add testing advanced

---
+++ testing-advanced/src/main/java/com.sda.testing.advanced/SpringTestApplication.java
@@ -0,0 +1,4 @@
+package com.sda.testing.advanced;
+
+public class SpringTestApplication {
+}
---
+++ testing-advanced/src/test/java/com/sda/testing/advanced/dto/BookMapperTest.java
@@ -0,0 +1,12 @@
+package com.sda.testing.advanced.dto;
+
+import org.junit.jupiter.api.Test;
+
+class BookMapperTest {
+
+    @Test
+    void givenEntity_whenToDto_thenReturnDto() {
+
+    }
+
+}
